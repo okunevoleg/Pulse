91810b9dd448013fcb59c3d6dd66584de8e18a76
Oleg <danilkurzik@gmail.com>
2021-07-30 19:41:09 +0600

    new__commit

---
+++ src/js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 800,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"</button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"</button>',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }
+          ]
+          });
+
+          $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+            $(this)
+              .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+              .closest('div.container').find('div.catalog__content').removeClass
+              ('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+          });
+
+        function toggleSlide(item){
+            $(item).each(function(i) {
+                $(this).on('click', function(e) {
+                    e.preventDefault();
+                    $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                    $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                })
+            })
+        };
+
+        toggleSlide('.catalog-item__link');
+        toggleSlide('.catalog-item__back');
+      });
